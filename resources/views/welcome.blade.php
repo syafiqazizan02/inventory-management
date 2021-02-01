2af4faa569c8b77cb795744de7c9387a3a75a7bd
SyafiqAz <developmentsyafiq@gmail.com>
2021-02-01 22:29:49 +0800

    app.css & app.js to vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="">
     <link href="{{ asset('backend/img/logo/logo.png') }}" rel="icon">
     <title>RuangAdmin - Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
     <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('backend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('backend/css/ruang-admin.min.css') }}" rel="stylesheet">
@@ -624,7 +625,7 @@
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
+<script src="{{ asset('js/app.js')}}"></script>
 <script src="{{ asset('backend/vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
